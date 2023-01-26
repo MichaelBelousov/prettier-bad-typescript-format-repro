1e677b9dcc3c57d561727768b93dde4280f1c623
Michael Belousov <mike.belousov@bentley.com>
2023-01-26 10:45:28 -0500

    simplified enough

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 export const foo = (
-      /// prettier-ignore
-      ( // eslint-disable-next-line @typescript-eslint/no-var-requires
-        require("some-really-long-thing-like-super-long") as typeof import("some-really-long-thing-like-super-long")
-      ).fooBarBaz.settings as any as FooBarBaz
-    ).qux;
+  // prettier-ignore
+  bar as Baz
+).qux;
