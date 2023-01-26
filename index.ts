c99ff64b1454dfc2c8c2f85fc7226039bfe8e8ea
Michael Belousov <mike.belousov@bentley.com>
2023-01-26 10:42:56 -0500

    remove excess stuff 1

--- index.ts
+++ index.ts
@@ -1,9 +1,5 @@
-export const foo = bar()
-  ? baz
-  : (
-      // FIXME: when we remove the ipcUtilityProcess, let's make getting+creating this settings object typed, because the current JSON
-      // type doesn't cast well to normal objects
-      // prettier-ignore
+export const foo = (
+      /// prettier-ignore
       ( // eslint-disable-next-line @typescript-eslint/no-var-requires
         require("some-really-long-thing-like-super-long") as typeof import("some-really-long-thing-like-super-long")
       ).fooBarBaz.settings as any as FooBarBaz
